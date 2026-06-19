5a412f253905234f80e0fc728ae0df96b9be9739
Ben Zadorian <124893639+BenjaminZadorian@users.noreply.github.com>
2026-06-19 18:30:36 +0100

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,16 +1,15 @@
 export const siteConfig = {
-  name: "Ryan Fitzgerald",
-  title: "Senior Software Engineer",
-  description: "Portfolio website of Ryan Fitzgerald",
-  accentColor: "#1d4ed8",
+  name: "Benjamin Zadorian",
+  title: "Software Engineer",
+  description: "Portfolio website of Benjamin Zadorian",
+  accentColor: "#9F2DE3",
   social: {
-    email: "your-email@example.com",
-    linkedin: "https://linkedin.com/in/yourprofile",
-    twitter: "https://x.com/rfitzio",
-    github: "https://github.com/RyanFitzgerald",
+    email: "zadorianben@protonmail.com",
+    linkedin: "https://www.linkedin.com/in/benzadorian/",
+    github: "https://github.com/BenjaminZadorian",
   },
   aboutMe:
-    "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem quos asperiores nihil consequatur tempore cupiditate architecto natus commodi corrupti quas quasi facere est, dignissimos odit nam veniam sapiente ut, vitae eligendi ipsum dolor, nostrum ullam impedit! Corrupti ratione mollitia temporibus necessitatibus, consectetur reiciendis recusandae id, dolorum quaerat, vero pariatur. Ratione!",
+    "",
   skills: ["Javascript", "React", "Node.js", "Python", "AWS", "Docker"],
   projects: [
     {
